f918b209a797cb5c75b1e0919e07d041f230c91d
ker4ik13 <kekustop228@mail.ru>
2024-01-25 21:43:54 +0300

    fix: исправлен емайл сервис

--- src/services/mail.service.ts
+++ src/services/mail.service.ts
@@ -1,13 +1,13 @@
-import nodemailer, { Transporter } from 'nodemailer';
-import { IClaim } from '../types/IClaim';
+import nodemailer, { Transporter } from "nodemailer";
+import { IClaim } from "../types/IClaim";
 
-const SMTP_HOST: string = process.env.SMTP_HOST || '';
+const SMTP_HOST: string = process.env.SMTP_HOST || "";
 const SMTP_PORT: number = Number(process.env.SMTP_PORT) || 0;
-const SMTP_USER: string = process.env.SMTP_USER || '';
-const SMTP_PASS: string = process.env.SMTP_PASS || '';
+const SMTP_USER: string = process.env.SMTP_USER || "";
+const SMTP_PASS: string = process.env.SMTP_PASS || "";
 
-if(!SMTP_HOST || !SMTP_PORT || !SMTP_USER || !SMTP_PASS) {
-  throw new Error('SMTP data error imported from .env');
+if (!SMTP_HOST || !SMTP_PORT || !SMTP_USER || !SMTP_PASS) {
+  throw new Error("SMTP data error imported from .env");
 }
 
 const connection = {
@@ -16,7 +16,6 @@ const connection = {
   user: SMTP_USER,
   pass: SMTP_PASS,
 };
-
 
 class MailService {
   transporter: Transporter;
@@ -32,51 +31,41 @@ class MailService {
         pass: connection.pass,
       },
       logger: true,
-    })
+    });
   }
 
-  async sendMailClaim (claim: IClaim) {
-
+  async sendMailClaim(claim: IClaim) {
     const result = await this.transporter.sendMail({
       from: `${connection.user}@yandex.ru`,
       to: `${connection.user}@yandex.ru`,
       subject: 'Новая заявка с сайта "Твоя Кухня"',
-      html:
-        `
+      html: `
           <div>
             <h1>Новая заявка!</h1>
             <p><b>Имя</b>: ${claim.firstName}</p>
-            ${claim.email ? `<p>Почта: <a href=${'mailto:' + claim.email}></a>${claim.email}</p>` : '<p><b>Почта</b>: Нет</p>'}
+            ${
+              claim.email
+                ? `<p>Почта: <a href=${"mailto:" + claim.email}></a>${
+                    claim.email
+                  }</p>`
+                : ""
+            }
             <p><b>Номер телефона</b>: ${claim.mobilePhone}</p>
-            <p><b>Дата заявки</b>: ${new Date(claim.date).toLocaleString('ru')}</p>
+            ${claim.tag ? `<p><b>Тег</b>: ${claim.tag}</p>` : ""}
+            ${
+              claim.location
+                ? `<p><b>Расположение формы</b>: ${claim.location}</p>`
+                : ""
+            }
+            <p><b>Дата заявки</b>: ${new Date(claim.date).toLocaleString(
+              "ru",
+            )}</p>
           </div>
-        `
-    })
+        `,
+    });
 
     console.log(result.response);
   }
-  
-  // async sendActivationMail (to: string, link: string) {
-  //   console.log(this.transporter.options);
-  //   console.log(connection);
-
-  //   const result = await this.transporter.sendMail({
-  //     from: connection.user,
-  //     to: to,
-  //     // sender: 'Твоя Кухня',
-  //     subject: 'Активация аккаунта на youkuhnya.ru',
-  //     text: '',
-  //     html:
-  //       `
-  //         <div>
-  //           <h1>Для активации перейдите по ссылке:</h1>
-  //           <a href="${link}">${link}</a>
-  //         </div>
-  //       `
-  //   })
-
-  //   console.log(result.response);
-  // }
 }
 
 export default new MailService();
